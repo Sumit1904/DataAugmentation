0373259dcba79d2635378b85e055e2515f91fe12
Sumit singh <86779553+Sumit1904@users.noreply.github.com>
2022-08-23 15:37:24 +0100

    Update __init__.py

--- DataAugmentation/__init__.py
+++ DataAugmentation/__init__.py
@@ -1,9 +1,10 @@
-from ImageAugmentation import *
-imgaug = ImageAugmentation('X_train', [0,1,1])
-final_lst = imgaug.dataAugmentation(directory = "modified_X_train", parent_dir = "/Users/sumitsingh/PycharmProjects/DataAugmentation",
-                                    resize='150x150', rotate=[90, 180, 270], scale=[1, 1.5, 1], noiseAmt=0.2,
-                         noiseSaltvsPepper=0.004, transformavalue=[0.09,0.43,0.56,0.85,0.99,0.32,0.32,0.99], flip=[0,1],
-                         kernel=(5,5),translation=[[1, 0, 25], [0, 1, 50]])
+from DataAugmentation.ImageAugmentation import ImageAugmentation
+# from ImageAugmentation import *
+# imgaug = ImageAugmentation('X_train', [0,1,1])
+# final_lst = imgaug.dataAugmentation(directory = "modified_X_train", parent_dir = "/Users/sumitsingh/PycharmProjects/DataAugmentation",
+#                                     resize='150x150', rotate=[90, 180, 270], scale=[1, 1.5, 1], noiseAmt=0.2,
+#                          noiseSaltvsPepper=0.004, transformavalue=[0.09,0.43,0.56,0.85,0.99,0.32,0.32,0.99], flip=[0,1],
+#                          kernel=(5,5),translation=[[1, 0, 25], [0, 1, 50]])
 
-imgaug.showImg(final_lst)
-imgaug.getY_trainValue()
+# imgaug.showImg(final_lst)
+# imgaug.getY_trainValue()
